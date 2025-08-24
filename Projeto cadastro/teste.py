bfe08e258a5432fa0c5a23ad6261f23a8020f23d
Arthur Tanaka <feragamerbox@gmail.com>
2025-08-23 21:59:01 -0300

    Update teste.py
    Arquivo com classe e funções CRUD

--- Projeto cadastro/teste.py
+++ Projeto cadastro/teste.py
@@ -1,5 +1,7 @@
 usuarios = {}
+
 class Opcoes:
+
     # Cadastrar Usuário
     def cadastrar_usuario(self, nome, idade, email):
         if nome in usuarios:
@@ -25,3 +27,39 @@ class Opcoes:
             print("Usuário não encontrado!")
         else:
             print("Dados do usuário:", usuarios[nome])
+
+    # Atualizar usuários
+    def atualizar_usuario(self, nome):
+        if nome not in usuarios:
+            print("Não foi possível atualizar o usuário, ele não existe.")
+            return
+
+        print("Se não quiser mudar algum dado, apenas aperte ENTER.")
+        novo_nome = input("Digite o novo nome: ")
+        nova_idade = input("Digite a nova idade: ")
+        novo_email = input("Digite o novo email: ")
+
+        if not novo_nome.strip():
+            novo_nome = nome
+        if not nova_idade.strip():
+            nova_idade = usuarios[nome]["idade"]
+        if not novo_email.strip():
+            novo_email = usuarios[nome]["email"]
+
+        if novo_nome != nome:
+            usuarios[novo_nome] = {"idade": nova_idade, "email": novo_email}
+            del usuarios[nome]
+        else:
+            usuarios[nome]["idade"] = nova_idade
+            usuarios[nome]["email"] = novo_email
+
+        print("Usuário atualizado com sucesso!")
+
+
+    # Deletar Usuário
+    def deletar_usuario(self, nome):
+        if nome in usuarios:
+            del usuarios[nome]
+            print(f"Usuario: {nome} deletado com sucesso!")
+        else:
+            print("Usuário não cadastrado / não encontrado")
